738262023372528272251caa0d1366202afece9d
Anthony Madhvani <anny.mdvn@gmail.com>
2016-01-31 22:18:39 +0100

    Add authentication util and CLI

--- utils/authentication.rb
+++ utils/authentication.rb
@@ -5,25 +5,32 @@ require 'yaml'
 class Encryption
   def Encryption.encrypt(text)
     api_data = YAML::load_stream(File.open("./api_data.yaml")).first["api"];
+
     # create the cipher for encrypting
     cipher = OpenSSL::Cipher::Cipher.new("aes-256-cbc")
     cipher.encrypt
 
-    # you will need to store these for later, in order to decrypt your data
-    key = Digest::SHA1.hexdigest("yourpass")
-    iv = cipher.random_iv
-
     # load them into the cipher
-    cipher.key = key
-    cipher.iv = iv
+    cipher.key = Digest::SHA1.hexdigest(api_data["secret"])
+    cipher.iv = api_data["iv"]
 
     # encrypt the message
-    encrypted = cipher.update('This is a secure message, meet at the clock-tower at dawn.')
+    encrypted = cipher.update(text)
     encrypted << cipher.final
-    puts "encrypted: #{encrypted}\n"
+    encrypted
   end
 
   def Encryption.decrypt(text)
     api_data = YAML::load_stream(File.open("./api_data.yaml")).first["api"];
+
+    cipher = OpenSSL::Cipher::Cipher.new("aes-256-cbc")
+    cipher.decrypt
+    cipher.key = Digest::SHA1.hexdigest(api_data["secret"])
+    cipher.iv = api_data["iv"]
+
+    # and decrypt it
+    decrypted = cipher.update(text)
+    decrypted << cipher.final
+    decrypted
   end
 end
