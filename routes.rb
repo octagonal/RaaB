aea08a10e955b5be9cbb06b38e7f802bd6819473
Anthony Madhvani <anny.mdvn@gmail.com>
2016-02-10 20:14:35 +0100

    Integrate login

--- routes.rb
+++ routes.rb
@@ -1,10 +1,27 @@
 require 'tilt/erubis'
 require 'sinatra'
+require 'haml'
+
 
 require './repository/Posts.rb'
 require './repository/Info.rb'
+require './repository/Api.rb'
+
+require "dm-core"
+require "dm-migrations"
+require "digest/sha1"
+require "sinatra-authentication"
+
+
+use Rack::Session::Cookie, :secret => Api.api_data["secret"]
+
+DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/test.db")
+DataMapper.auto_upgrade!
 
 get '/' do
+  if logged_in?
+    puts current_user.email
+  end
 
   erb :index, :locals => {
     :posts => Posts.getAll(),
@@ -12,18 +29,16 @@ get '/' do
   }
 end
 
-
-get '/:id/:title' do
+get '/flair/:tag' do
   erb :index, :locals => {
-    :posts => Posts.getSingle("#{params['id']}"),
+    :posts => Posts.getByFlair("#{params['tag']}"),
     :sidebar => Info.getSidebar()
   }
 end
 
-
-get '/:flair' do
+get '/:id/:title' do
   erb :index, :locals => {
-    :posts => Posts.getByFlair("#{params['flair']}"),
+    :posts => Posts.getSingle("#{params['id']}"),
     :sidebar => Info.getSidebar()
   }
 end
